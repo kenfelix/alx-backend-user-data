dc831b6fc1fd1b5188c18bc18ea0d3ab02ed3189
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-12 16:43:36 +0100

    Create encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Encrypt passwords"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """returns a salted, hashed password, which is a byte string."""
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
