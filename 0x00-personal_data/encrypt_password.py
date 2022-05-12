6c517c0b9ef5d2fa04e8fd7bec2c762f36143475
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-12 20:56:44 +0100

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,4 +10,4 @@ def hash_password(password: str) -> bytes:
 
 def is_valid(hashed_password: Callable, password: str) -> bool:
     """check if password is valid"""
-    return bcrypt.checkpw(password.encode(), hashed_password)
+    return True if bcrypt.checkpw(password.encode(), hashed_password) else False
