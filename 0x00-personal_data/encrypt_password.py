7e36e7e020256efd015ed4e3029fef23450d6672
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-12 20:59:46 +0100

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """ Encrypt passwords"""
+
 import bcrypt
-from typing import Callable
 
 
 def hash_password(password: str) -> bytes:
     """returns a salted, hashed password, which is a byte string."""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
-def is_valid(hashed_password: Callable, password: str) -> bool:
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """check if password is valid"""
     return bcrypt.checkpw(password.encode(), hashed_password)
