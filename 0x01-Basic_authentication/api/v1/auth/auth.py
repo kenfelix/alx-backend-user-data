8f330ff8388ab095ab6a9ffa6b40209669ad02d2
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-17 14:35:33 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,13 +11,18 @@ class Auth:
     
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """retuens false"""
-        if path in excluded_paths:
-            return False
         if not path or not excluded_paths:
             return True
+        if path in excluded_paths:
+            return False
         if path[-1] != '/' and path + '/'\
                 in excluded_paths:
             return False
+        for p in excluded_paths:
+            if p.endswith('*') and\
+                    path.startswith(p[:-1]):
+                return False
+        return True
         
     
     def authorization_header(self, request=None) -> str:
