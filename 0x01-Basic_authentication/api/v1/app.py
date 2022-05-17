d5902516cc1c0b2cc8357dff1d911f3376199f17
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-17 14:52:58 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
 
 
 app = Flask(__name__)
@@ -20,6 +21,26 @@ if getenv('AUTH_TYPE') == 'basic_auth':
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
+    
+
+@app.before_request
+def before_request() -> None:
+    """
+    Executed before each request
+    that is handled by a function
+    of that blueprint
+    """
+    excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+            ]
+    if auth:
+        if auth.require_auth(request.path, excluded_paths):
+            if not auth.authorization_header(request):
+                abort(401)
+            if not auth.current_user(request):
+                abort(403)
 
 
 @app.errorhandler(404)
