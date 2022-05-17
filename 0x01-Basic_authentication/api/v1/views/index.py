04c69a03e9c60e1a35c7c10b0837ed9ce6f96242
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-17 13:00:29 +0100

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_veiws.route('/unauthorized/', strict_slashes=False)
+@app_views.route("/unauthorized/", strict_slashes=False)
 def unauthorized() -> str:
     """Tests the 401 error handler"""
     abort(401)
