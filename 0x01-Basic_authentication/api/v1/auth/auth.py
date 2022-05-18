2ecce6706d4ccd936ea21d2e542f6e9bc1a53f47
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-18 13:30:50 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,7 @@ from typing import List, TypeVar
 
 class Auth:
     """AUth Class"""
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """retuens false"""
         if not path or not excluded_paths:
@@ -23,14 +23,14 @@ class Auth:
                     path.startswith(p[:-1]):
                 return False
         return True
-        
-    
+
+
     def authorization_header(self, request=None) -> str:
         """returns None"""
         if not request or not request.headers or not request.headers.get('Authorization'):
             return None
         return request.headers.get('Authorization')
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """return None"""
         return None
