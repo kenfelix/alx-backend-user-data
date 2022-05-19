3dcf42b73ab022ebb742be8cf19d2f93a75a7df3
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-19 20:13:14 +0100

    Create session_auth.py

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module session_auth
+"""
+from api.v1.auth.auth import Auth
+import uuid
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """Session authentication mechanism"""
+    pass
