723ea486d71b1b26bc10a00bbb6111f5a5712fe8
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-19 21:09:55 +0100

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,13 +39,12 @@ def before_request() -> None:
             '/api/v1/forbidden/'
             '/api/v1/auth_session/login/'
             ]
-    if auth:
-        if auth.require_auth(request.path, excluded_paths):
-            if not auth.authorization_header(request)\
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if not auth.authorization_header(request)\
                 and not auth.session_cookie(request):
             abort(401)
-            if not auth.current_user(request):
-                abort(403)
+        if not auth.current_user(request):
+            abort(403)
         request.current_user = auth.current_user(request)
 
 
