e4055cb56c09bc5bfad638aeeba4c71a3be63e5a
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-19 21:08:17 +0100

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -41,8 +41,9 @@ def before_request() -> None:
             ]
     if auth:
         if auth.require_auth(request.path, excluded_paths):
-            if not auth.authorization_header(request):
-                abort(401)
+            if not auth.authorization_header(request)\
+                and not auth.session_cookie(request):
+            abort(401)
             if not auth.current_user(request):
                 abort(403)
         request.current_user = auth.current_user(request)
