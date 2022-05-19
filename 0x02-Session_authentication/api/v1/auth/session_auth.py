861b6793f6797b55ac3a8d5734da58f1db55fc7f
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-19 20:34:32 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,4 +9,12 @@ from models.user import User
 
 class SessionAuth(Auth):
     """Session authentication mechanism"""
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if not user_id or type(user_id) != str:
+            return None
+        session_id = str(uuid.uuid4())
+        user_id_by_session_id[session_id] = user_id
