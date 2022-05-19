696571d0590abfa7e5820633690938b6831e67f9
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-19 20:09:16 +0100

    Update users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -32,7 +32,7 @@ def view_one_user(user_id: str = None) -> str:
         if request.current_user is None:
             abort(404)
         return jsonify(request.current_user.to_json())
-    
+
     if user is None:
         abort(404)
     return jsonify(user.to_json())
