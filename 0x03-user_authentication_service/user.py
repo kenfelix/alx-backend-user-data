5b02e180119c1b906cab46f62bb65d7a6815e9ec
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-24 12:32:53 +0100

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,7 +4,16 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
+
 Base = declarative_base()
 
 
-class User():
+class User(Base):
+    """SQLAlchemy model using the mapping declaration of SQLAlchemy"""
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
