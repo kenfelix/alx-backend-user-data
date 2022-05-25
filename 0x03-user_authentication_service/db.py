c2a949dbec706c8ab02d026e78ae56a98076ad90
kenfelix <kmrapper.kf@gmail.com>
2022-05-25 20:22:22 +0000

    next commit

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,11 +31,11 @@ class DB:
             self.__session = DBSession()
         return self.__session
     
-    def add_user(email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """saves the user to the database and return a user obj"""
         new_user = User(email=email, hashed_password=hashed_password)
-        self.__session.add(new_user)
-        self.__session.commit(new_user)
+        self._session.add(new_user)
+        self._session.commit()
         return new_user
     
     def find_user_by(self, **kwargs) -> User:
