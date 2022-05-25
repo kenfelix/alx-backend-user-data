6a977209390823e4591179e7f156685888c1b08f
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-25 22:05:16 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,7 @@
 from bcrypt import hashpw, gensalt, checkpw
 
 
-def _hash_password(password: str) => bytes:
+def _hash_password(password: str) -> bytes:
     """Returns a salted hash of
     the input password"""
     hashed_password = hashpw(password.encode('utf-8'), gensalt())
