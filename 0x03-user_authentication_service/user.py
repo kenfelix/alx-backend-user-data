a8132b173a78d082a05deeb84183619b4454c1c9
kenfelix <kmrapper.kf@gmail.com>
2022-05-25 20:00:50 +0000

    next commit

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,9 +11,10 @@ Base = declarative_base()
 class User(Base):
     """SQLAlchemy model using the mapping declaration of SQLAlchemy"""
     __tablename__ = 'user'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
