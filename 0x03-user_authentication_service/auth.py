f751442bde3cb044b22c7e3aaabb04a15a679985
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-25 22:02:00 +0100

    Create auth.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Auth Module"""
+from bcrypt import hashpw, gensalt, checkpw
+
+
+def _hash_password(password: str) => bytes:
+    """Returns a salted hash of
+    the input password"""
+    hashed_password = hashpw(password.encoding('utf-8'), gensalt())
+
+    return hashed_password
